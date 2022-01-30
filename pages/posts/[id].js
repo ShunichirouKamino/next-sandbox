21a4c51efb022c7f43ddf86a7389498144ac5eed
ShunichirouKamino <kamino.shunichirou@isid.co.jp>
2022-01-30 22:20:29 +0900

    動的ルーティング

---
+++ pages/posts/[id].js
@@ -0,0 +1,17 @@
+import Layout from "../../components/layout";
+import { getAllPostIds } from "../../lib/posts";
+
+export default function Post() {
+  return <Layout>...</Layout>;
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+export async function getStaticProps({ params }) {
+  // params.id を使用して、ブログの投稿に必要なデータを取得する
+}
