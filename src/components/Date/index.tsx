5ba9d058290c99c0db085561a0d6a8e13db0cf9d
ShunichirouKamino <kamino.shunichirou@isid.co.jp>
2022-02-16 00:16:08 +0900

    dateコンポーネント拡張

--- src/components/Date/index.tsx
+++ src/components/Date/index.tsx
@@ -1,8 +1,17 @@
 import { parseISO, format } from "date-fns";
 
-const Date = ({ dateString }) => {
-  const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, "LLLL d, yyyy")}</time>;
+type Props = {
+  dateString: string;
+};
+const Date: React.FC<Props> = ({ dateString }) => {
+  const date: Date = parseISO(dateString);
+  return (
+    <>
+      <div className="p-2 pr-8 text-right">
+        <time dateTime={dateString}>{format(date, "LLLL d, yyyy")}</time>
+      </div>
+    </>
+  );
 };
 
 export default Date;
