c78d048208b40f3bcc72d74534dccccf33579f55
ShunichirouKamino <kamino.shunichirou@isid.co.jp>
2022-02-16 00:00:41 +0900

    simpleテキストコンポーネントの拡張

--- src/components/SimpleText/index.tsx
+++ src/components/SimpleText/index.tsx
@@ -1,10 +1,11 @@
 type Props = {
   text: string;
   align: "text-left" | "text-center" | "text-right" | "text-justify";
+  size?: "text-xs" | "text-base" | "text-2xl" | "text-5xl";
 };
 
-const SimpleText: React.FC<Props> = ({ text, align }) => {
-  return <div className={`${align}`}>{text}</div>;
+const SimpleText: React.FC<Props> = ({ text, align, size = "text-base" }) => {
+  return <div className={`${align} ${size}`}>{text}</div>;
 };
 
 export default SimpleText;
