54b51369a62fe9ec1d657edb15cfbff457f37d4c
ShunichirouKamino <kamino.shunichirou@isid.co.jp>
2022-02-19 17:07:21 +0900

    table可変にした

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -15,62 +15,49 @@ export type TableProps<R> = {
 };
 
 function Table<R>({ columns, rawdata }: TableProps<R>): JSX.Element {
+  console.log(rawdata[0][`${columns[0].label.toLowerCase()}`]);
   return (
     <>
       <div className="p-10">
         <table className="frex w-full table-auto rounded-lg sm:shadow-lg">
           <thead className="text-white">
             <tr className="bg-[#1e3da3]">
-              <th className="p-3 text-left w-1/3">Name</th>
-              <th className="p-3 text-left w-1/3">Email</th>
-              <th className="p-3 text-left w-1/3">Actions</th>
+              {columns.map((col, index) => (
+                <th className={`${col.size}` + " p-3 text-left"}>
+                  {col.label}
+                </th>
+              ))}
             </tr>
           </thead>
-          <tbody className="flex-1 sm:flex-none">
-            <tr className="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
-              <td className="border-grey-light border hover:bg-gray-100 p-3">
-                John Covv
-              </td>
-              <td className="border-grey-light border hover:bg-gray-100 p-3 truncate">
+          <tbody>
+            {rawdata.map((rawdata) => (
+              <tr>
+                {columns.map((col) => (
+                  <td className="border hover:bg-gray-100 p-3">
+                    {rawdata[`${col.label.toLowerCase()}`]}
+                  </td>
+                ))}
+              </tr>
+            ))}
+
+            {/* <tr>
+              <td className="border hover:bg-gray-100 p-3">John Covv</td>
+              <td className="border hover:bg-gray-100 p-3">
                 contato@johncovv.com
               </td>
-              <td className="border-grey-light border hover:bg-gray-100 p-3 text-red-400 hover:text-[#1e3da3] hover:font-medium cursor-pointer">
+              <td className="border hover:bg-gray-100 p-3 text-red-400 hover:text-[#1e3da3] hover:font-medium cursor-pointer">
                 Delete
               </td>
             </tr>
-            <tr className="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
-              <td className="border-grey-light border hover:bg-gray-100 p-3">
-                Michael Jackson
-              </td>
-              <td className="border-grey-light border hover:bg-gray-100 p-3 truncate">
+            <tr>
+              <td className="border hover:bg-gray-100 p-3">Michael Jackson</td>
+              <td className="border hover:bg-gray-100 p-3">
                 m_jackson@mail.com
               </td>
-              <td className="border-grey-light border hover:bg-gray-100 p-3 text-red-400 hover:text-[#1e3da3] hover:font-medium cursor-pointer">
+              <td className="border hover:bg-gray-100 p-3 text-red-400 hover:text-[#1e3da3] hover:font-medium cursor-pointer">
                 Delete
               </td>
-            </tr>
-            <tr className="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
-              <td className="border-grey-light border hover:bg-gray-100 p-3">
-                Julia
-              </td>
-              <td className="border-grey-light border hover:bg-gray-100 p-3 truncate">
-                julia@mail.com
-              </td>
-              <td className="border-grey-light border hover:bg-gray-100 p-3 text-red-400 hover:text-[#1e3da3] hover:font-medium cursor-pointer">
-                Delete
-              </td>
-            </tr>
-            <tr className="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
-              <td className="border-grey-light border hover:bg-gray-100 p-3">
-                Martin Madrazo
-              </td>
-              <td className="border-grey-light border hover:bg-gray-100 p-3 truncate">
-                martin.madrazo@mail.com
-              </td>
-              <td className="border-grey-light border hover:bg-gray-100 p-3 text-red-400 hover:text-[#1e3da3] hover:font-medium cursor-pointer">
-                Delete
-              </td>
-            </tr>
+            </tr> */}
           </tbody>
         </table>
       </div>
--- src/components/TopPage/index.tsx
+++ src/components/TopPage/index.tsx
@@ -1,7 +1,7 @@
 import Header from "../Header";
 import ImageContainer from "../ImageContainer";
 import SideBar from "../SideBar";
-import Table from "../Table";
+import Table, { Column } from "../Table";
 import Body from "./Body/Body";
 
 export type TopPageProps = {
@@ -9,13 +9,25 @@ export type TopPageProps = {
 };
 
 const TopPage: React.FC<TopPageProps> = ({ text }) => {
+  const columns = [
+    { label: "Name", size: "1/3" },
+    { label: "Email", size: "1/3" },
+    { label: "Action", size: "1/3" },
+  ];
+
+  const rawData = [
+    { name: "Shunichirou", email: "mail1@gmail.com", action: "Delete" },
+    { name: "Taro", email: "mail2@gmail.com", action: "Delete" },
+    { name: "Ziro", email: "mail3@gmail.com", action: "Delete" },
+  ];
+
   return (
     <>
       <div className="flex">
         <SideBar></SideBar>
         <main className=" min-h-screen w-full">
           <Header></Header>
-          <Table columns={[]}></Table>
+          <Table columns={columns} rawdata={rawData}></Table>
           <Body></Body>
         </main>
       </div>
