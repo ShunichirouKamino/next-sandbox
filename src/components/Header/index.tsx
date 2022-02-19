af9ff7afe10112dc9f57c62a88fe4f71f5f16b92
ShunichirouKamino <kamino.shunichirou@isid.co.jp>
2022-02-19 23:01:01 +0900

    header固定

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,7 +8,7 @@ export type HeaderProps = {};
 const Header: React.FC<HeaderProps> = () => {
   return (
     <>
-      <nav className="flex justify-between items-center px-10 bg-white py-6">
+      <nav className="flex justify-between items-center px-10 bg-white py-6 sticky top-0">
         <SearchBox></SearchBox>
         <div className="flex items-center">
           <SimpleText
