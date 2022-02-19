b341e84be7aa4a438fe882b6870c9723392ec5e3
ShunichirouKamino <kamino.shunichirou@isid.co.jp>
2022-02-19 23:01:08 +0900

    不要な変数削除

--- src/components/ReesultPage/index.tsx
+++ src/components/ReesultPage/index.tsx
@@ -4,21 +4,17 @@ import SimpleText from "../SimpleText";
 import Table, { TableProps } from "../Table";
 import Body from "./Body/Body";
 
-type Result = {
-  userA;
-  userB;
-  userC;
-  userD;
-  userE;
-  userF;
-};
-
 export type ResultPageProps = {
   members: string[];
   results: string[][];
 };
 
 const ResultPage: React.FC<ResultPageProps> = ({ members, results }) => {
+  /**
+   * style sample
+   * bodyStyle:
+   *       "text-blue-400 hover:text-[#1e3da3] hover:font-medium cursor-pointer",
+   */
   const columns = members.map((m) => {
     const c: string = m;
     return { label: c, size: "w-1/6" };
@@ -34,24 +30,6 @@ const ResultPage: React.FC<ResultPageProps> = ({ members, results }) => {
       Shunichiro: raw[5],
     };
   });
-  console.log(rawData);
-
-  // const columns = [
-  //   { label: "Name", size: "w-1/3" },
-  //   { label: "Email", size: "w-1/3" },
-  //   {
-  //     label: "Action",
-  //     size: "w-1/3",
-  //     bodyStyle:
-  //       "text-blue-400 hover:text-[#1e3da3] hover:font-medium cursor-pointer",
-  //   },
-  // ];
-
-  // const rawData = [
-  //   { name: "Shunichirou", email: "mail1@gmail.com", action: "Delete" },
-  //   { name: "Taro", email: "mail2@gmail.com", action: "Delete" },
-  //   { name: "Ziro", email: "mail3@gmail.com", action: "Delete" },
-  // ];
 
   return (
     <>
